e3c4eb28ca9c9b59c21401c7afa25b662a3020af
HEARELMER <hearelmer2005@gmail.com>
2025-05-10 19:33:19 -0500

    fix: add missing newline at end of file in main function

--- MyHttpFunction/__init__.py
+++ MyHttpFunction/__init__.py
@@ -2,4 +2,5 @@ import logging
 import azure.functions as func
 
 def main(mytimer: func.TimerRequest) -> None:
-    logging.info('¡Hola Mundo! Esta es una función de Azure ejecutada correctamente.')
+    logging.info('¡Hola Mundo! Esta es una función de Azure ejecutada correctamente. :)')
+    
